221dd642a22d174bee661bce28c766043190d201
shruthi093 <jayasruthi.l@gmail.com>
2022-08-25 15:35:19 +0530

    Cookies

--- Auth.js
+++ Auth.js
@@ -5,8 +5,10 @@ const jwt=require('jsonwebtoken')
 const userCredentials=require('./data/credentials')
 const cors=require('cors')
 const authorize=require('./middleware/authorize')
-app.use(cors())
+const cookieparser=require('cookie-parser')
+app.use(cors({origin:'http://localhost:3000',credentials:true}))
 app.use(bp.json())
+app.use(cookieparser())
 
 app.post('/signin',(req,res)=>{
 const data=req.body
@@ -19,11 +21,31 @@ else{
     res.send({"msg":' not authenticated',"status":false})
 }
 })
+app.post('/login',(req,res)=>{
+    const data=req.body
+    const result=userCredentials.find((item)=>item.email===data.email)
+    if(data.password===result.password){
+        const token=jwt.sign({email:data.email},'jamesbond')
+        console.log(token)
+        res.status(200).cookie('sometokevalue',token,{httpOnly:true}).send({"msg":'you are authenticated',"status":true})   
+    }
+    else{
+        res.send({"msg":' not authenticated',"status":false})
+    }
+})
+
 app.post('/delete',authorize,(req,res)=>{
     res.send({"msg":'deleted successfully'})
 })
 app.post('/update',authorize,(req,res)=>{
+
    res.send({"msg":"you are authorized"})
+})
+
+app.post('/seecookie',(req,res)=>{
+    console.log(req.cookies)
+res.send('cookie is seen')
+   //res.clearCookies().send("cookie is seen!")
 })
 
 app.listen(3001,()=>console.log('server started at port 3001'))
